2d308dbfdfafb68a8cfc7b3fd1e03dce4088159e
Glitch (node-sanitize-options) <none>
2019-02-11 16:23:08 +0000

    🐉🏘 Updated with Glitch

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ exports.options = function(passedOptions, defaultOptions) {
   if (typeof defaultOptions !== "undefined") {
     for (var key in defaultOptions) options[key] = defaultOptions[key];
   }
-  var options = {};
   if (typeof passedOptions !== "undefined") {
     for (var key in passedOptions) options[key] = passedOptions[key];
   }
